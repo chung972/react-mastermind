83bb2b07bcd11de08da7d91e65a38aca3d03c71c
Albert Chung <chung972@gmail.com>
2019-06-26 16:53:11 -0500

    adds constructor; getNewGuess(); genCode(); renders button; passes props

--- src/App.js
+++ src/App.js
@@ -6,22 +6,63 @@ import ColorPicker from "./components/ColorPicker/ColorPicker";
 import GameTimer from "./components/GameTimer/GameTimer";
 import NewGameButton from "./components/NewGameButton/NewGameButton";
 
+const colors = ['#7CCCE5', '#FDE47F', '#E04644', '#B576AD'];
+
+// we can inherit Component like class App extends React.Component, OR do this below:
+// import React, { Component} from "react";
 class App extends Component {
-  // we can inherit Component like class App extends React.Component, OR do this below:
-  // import React, { Component} from "react";
+  constructor() {
+    // super must be called before accessing 'this'
+    super();
+    this.state = {
+      // state is an object that holds information in its properties
+      // to UPDATE the STATE, you need to call on this.setState(); not like this.state.key="something"
+      selColorIdx: 0,
+      guesses: [this.getNewGuess(), this.getNewGuess()],
+      // for development purposes, let's initialize with TWO guess objects
+      code: this.genCode()
+    }
+  }
+
+  genCode() {
+    return new Array(4).fill().map(() => Math.floor(Math.random() * colors.length));
+    // NOTE: we are HARD CODING the length of the array; this will need to change as we 
+    // implement higher difficulties
+  }
+
+  getNewGuess() {
+    return {
+      // code: [null, null, null, null],    // comment out until done testing
+      code: [3, 2, 1, 0], // testing purposes
+      score: {
+        perfect: 0,
+        almost: 0
+      }
+    };
+  }
+
 
   // normally, you START off with having FUNCTIONAL components, and then
   // refactor as you run into cases where a component needs to hold STATE
+
   render() {
     return (
       <div className="App">
+        <button onClick={() => this.setState((state) => {
+          return {
+            selColorIdx: ++state.selColorIdx % 4
+          };
+        })}>
+          Next Color
+        </button>
+        Selected color: {colors[this.state.selColorIdx]}
         <header className="App-header">React Mastermind</header>
         <div className="flex-h">
-          <GameBoard />
+          <GameBoard colors={colors} guesses={this.state.guesses} />
           {/* remember, we DON'T want to try and add className (for styling) to GameBoard HERE;
             gotta go to the actual component file and do it there  */}
           <div>
-            <ColorPicker />
+            <ColorPicker colors={colors} selColorIdx={this.state.selColorIdx}/>
             <GameTimer />
             <NewGameButton />
           </div>
