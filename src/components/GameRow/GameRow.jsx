de53c03a741279fab5344d4c2ce2c746a872cc69
Albert Chung <chung972@gmail.com>
2019-06-26 12:53:44 -0500

    imports and returns GuessPegs

--- src/components/GameRow/GameRow.jsx
+++ src/components/GameRow/GameRow.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import GuessPegs from "../GuessPegs/GuessPegs";
 
 const GameRow = (props) => (
     <div className="component">
         GameRow
+        <GuessPegs />
     </div>
 );
 
