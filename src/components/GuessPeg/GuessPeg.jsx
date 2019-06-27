16931e81495181a83cea9030c9985fe4cc2efa74
Albert Chung <chung972@gmail.com>
2019-06-27 18:06:53 -0500

    imports css file;changes div to button tag; adds logic to color buttons dynamically

--- src/components/GuessPeg/GuessPeg.jsx
+++ src/components/GuessPeg/GuessPeg.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import styles from "./GuessPeg.module.css";
 
 const GuessPeg = (props) => (
-    <div>
-        {props.color}
-    </div>
+    <button 
+        className={styles.button}
+        style={{
+            backgroundColor: `${props.color}`,
+            borderColor: props.color
+        }} />
 );
 
 export default GuessPeg;
