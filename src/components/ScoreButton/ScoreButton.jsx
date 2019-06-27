eed28769d7fbe4580de93596756b59f38e28a3e8
Albert Chung <chung972@gmail.com>
2019-06-27 18:10:07 -0500

    imports css file and adds className to button

--- src/components/ScoreButton/ScoreButton.jsx
+++ src/components/ScoreButton/ScoreButton.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./ScoreButton.module.css";
 
 const ScoreButton = (props) => (
-    <button>
-        ScoreButton
+    <button className={`${styles.button} btn btn-default`}>
+        ✔
     </button>
 );
 
