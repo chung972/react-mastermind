5cd871712428fc49c1fc23710865e81964d3603e
Albert Chung <chung972@gmail.com>
2019-06-28 11:05:38 -0500

    imports module.css for GuessRow

--- src/components/GuessRow/GuessRow.jsx
+++ src/components/GuessRow/GuessRow.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import GuessPegs from "../GuessPegs/GuessPegs";
 import GuessScore from "../GuessScore/GuessScore";
 import ScoreButton from "../ScoreButton/ScoreButton";
-import "./GuessRow.css";
+import styles from "./GuessRow.module.css";
 
 const GuessRow = (props) => (
-    <div className='GuessRow'>
-        <div style={{color: props.currentGuess ? 'black' : 'lightgrey'}}>{props.rowIdx + 1}</div>
+    <div className={styles.GuessRow}>
+        <div className={styles.rowNum} style={{color: props.currentGuess ? 'black' : 'lightgrey'}}>{props.rowIdx + 1}</div>
         {/* because we can't access the key prop, we have to pass in a different prop
             this IS redundant, but necessary; also, we're adding 1 to be 1-based */}
         <GuessPegs 
