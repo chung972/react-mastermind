08bd2832bb906b06f082f224e5beb6f01906d4da
Albert Chung <chung972@gmail.com>
2019-06-30 16:52:19 -0500

    rearranges attribute s on button

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -5,14 +5,14 @@ const ColorPicker = (props) => (
     <div className={styles.ColorPicker}>
         {props.colors.map((color, index) => 
             <button 
-                color={color}
                 className={styles.button}
                 style={{
                     backgroundColor: props.selColorIdx === index ? 'white' : color,
                     borderColor: color
                 }}
-                key={color}
+                color={color}
                 onClick={()=>props.handleColorSelection(index)}
+                key={color}
                 // when you need to pass in an argument, just wrap the function that requires the args
                 // with a arrow function (you can even JUST have the empty parens and =>)
 
