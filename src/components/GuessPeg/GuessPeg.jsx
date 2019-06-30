56f179fd46ce361efa56937ef87981fa9882d6f8
Albert Chung <chung972@gmail.com>
2019-06-30 16:53:48 -0500

    adds comments; adds logic to dynamically display different styles based on props

--- src/components/GuessPeg/GuessPeg.jsx
+++ src/components/GuessPeg/GuessPeg.jsx
@@ -5,8 +5,11 @@ const GuessPeg = (props) => (
     <button 
         className={styles.peg}
         style={{
-            backgroundColor: props.color,
-            border: props.color ? `1px solid ${props.color}` : '1px dashed gray',
+            backgroundColor: props.code[props.index] ? "white" : props.color[props.code[props.index]],
+            border: props.code[props.index] ? `1px solid ${props.color}` : '1px dashed gray',
+            // gotta use props.code as the conditional; remember, for a NEW guess, each element
+            // in the guess code array is set to null; so, IF that code is NOT null, color the border
+            // to be the color of whatever. ELSE, if code === null, then leave the border to be gray dashed
             cursor: props.currentGuess && 'pointer'
             // remember, having the logical AND is like having an in-line if; progress through stack
         }} />
